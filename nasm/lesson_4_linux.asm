a9a226df9e26f40a5b41363b3350c2c955c223c2
oldsensei07 <oldsensei07@gmail.com>
2018-09-25 00:54:46 +0300

    Add 4rd lesson

---
+++ nasm/lesson_4_linux.asm
@@ -0,0 +1,22 @@
+	section .text
+	global _start
+_start:
+	mov edx, 10
+	mov ecx, name
+	mov ebx, 1
+	mov eax, 4
+	int 80h
+
+	mov [name], dword 'Nuha'
+
+	mov edx, 10
+	mov ecx, name
+	mov ebx, 1
+	mov eax, 4
+	int 80h
+
+	mov eax, 1
+	int 80h
+
+	section .data
+	name db 'Zara Ali ', 0xa
