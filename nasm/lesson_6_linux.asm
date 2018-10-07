5963b78da0ea1670b9abf952bf24b644c2e4433d
oldsensei07 <oldsensei07@gmail.com>
2018-10-07 21:06:44 +0300

    Added multiplying and dividing

--- nasm/lesson_6_linux.asm
+++ nasm/lesson_6_linux.asm
@@ -16,13 +16,13 @@ _start:
 	mov eax, SYS_WRITE
 	mov ebx, STDOUT
 	
-	mov ecx, [value]
-	add ecx, '0'
-	mov [prtVal], ecx
-	mov ecx, prtVal
+	add byte [value], '0'
+	mov ecx, value
 	mov edx, 1
 	int 80h
 
+	
+	sub WORD [value], '0' 	; stuppid but just for example
 	
 	dec WORD [value]
 	add byte [value], '0'
@@ -33,6 +33,35 @@ _start:
 	mov edx, 1
 	int 0x80
 
+	;; multiplication
+
+	mov al, 3
+	mov bl, 2
+	mul bl
+	
+	mov byte [value], al
+	add byte [value], '0'
+	
+	mov eax, SYS_WRITE
+	mov ebx, STDOUT
+	mov ecx, value
+	mov edx, 1
+	int 0x80
+
+	;; divide
+
+	mov ax, 16
+	mov bl, 8
+	div bl
+	mov [value], al
+	add byte [value], '0'
+	
+	mov eax, SYS_WRITE
+	mov ebx, STDOUT
+	mov ecx, value
+	mov edx, 1
+	int 0x80
+	
 	mov eax, SYS_EXIT
 	int 80h
 	
