fedced9ba283b5640b8e0780f23db23e9f2a5895
oldsensei07 <oldsensei07@gmail.com>
2018-11-20 01:04:09 +0300

    Added jz and checking for odd/even

--- nasm/lesson_7_linux.asm
+++ nasm/lesson_7_linux.asm
@@ -5,8 +5,15 @@
 	STDOUT equ 1
 
 	section .data
+	
 	pattern dw 0x03
-
+	
+	oddstring db 'this is odd', 0xa
+	oddstringLen equ $ - oddstring
+	
+	evenstring db 'this is even', 0xa
+	evenstringLen equ $ - oddstring
+	
 	section .text
 	global _start
 	
@@ -19,5 +26,23 @@ _start:
 	mov edx, 1
 	int 0x80
 
+	mov WORD eax, 9h
+	and eax, 0x1
+	jz evnn
+	
+	mov eax, SYS_WRITE
+	mov ebx, STDOUT
+	mov ecx, oddstring
+	mov edx, oddstringLen
+	int 0x80
+	jmp exit
+evnn:
+	mov eax, SYS_WRITE
+	mov ebx, STDOUT
+	mov ecx, evenstring
+	mov edx, evenstringLen
+	int 0x80
+	
+exit:	
 	mov eax, SYS_EXIT
 	int 0x80
