3ac532c723fde0795ce0a6af0a7415124b7b4eb8
oldsensei07 <oldsensei07@gmail.com>
2018-12-03 00:36:09 +0300

    Added xor

--- nasm/lesson_7_linux.asm
+++ nasm/lesson_7_linux.asm
@@ -13,7 +13,13 @@
 	
 	evenstring db 'this is even', 0xa
 	evenstringLen equ $ - oddstring
-	
+
+	messageForBitwiseOr db 'Result of 1000 OR 0001:', 0xa
+	messageLen equ $ - messageForBitwiseOr	
+
+	messageXoring db 'Result of 1010 XOR 1100:', 0xa
+	messageXoringLen equ $ - messageXoring	
+
 	section .text
 	global _start
 	
@@ -26,16 +32,17 @@ _start:
 	mov edx, 1
 	int 0x80
 
-	mov WORD eax, 9h
+	mov eax, 9h
 	and eax, 0x1
 	jz evnn
 	
 	mov eax, SYS_WRITE
 	mov ebx, STDOUT
+	
 	mov ecx, oddstring
 	mov edx, oddstringLen
 	int 0x80
-	jmp exit
+	jmp next
 evnn:
 	mov eax, SYS_WRITE
 	mov ebx, STDOUT
@@ -43,6 +50,50 @@ evnn:
 	mov edx, evenstringLen
 	int 0x80
 	
-exit:	
+next:
+;;; or operation
+	mov eax, SYS_WRITE
+	mov ebx, STDOUT
+	mov ecx, messageForBitwiseOr
+	mov edx, messageLen
+	int 0x80
+	
+	mov eax, 1000b
+	mov ebx, 0001b
+	or eax, ebx
+	add eax, '0'
+	
+	mov [result], eax
+	mov BYTE [result + 1], 0xa
+	
+	mov eax, SYS_WRITE
+	mov ebx, STDOUT
+	mov ecx, result
+	mov edx, 2
+	int 0x80
+
+	mov al, 1010b
+	mov bl, 1100b
+	xor al, bl
+	add al, '0'
+	mov BYTE [result], al
+	mov BYTE [result + 1], 0xa
+	
+	mov eax, SYS_WRITE
+	mov ebx, STDOUT
+	mov ecx, messageXoring
+	mov edx, messageXoringLen
+	int 0x80
+
+	mov eax, SYS_WRITE
+	mov ebx, STDOUT
+	mov ecx, result
+	mov edx, 2
+	int 0x80
+
+;;; exit
 	mov eax, SYS_EXIT
 	int 0x80
+
+	section .bss
+	result resd 1
